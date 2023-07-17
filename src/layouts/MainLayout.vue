6746fb173dbbbb748b702179cc44fcb75c573c65
Oleksii <kravchenko.a.a.rb@gmail.com>
2023-07-17 23:19:40 +0300

    add date filter

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -13,9 +13,13 @@
       </main>
 
       <div class="fixed-action-btn">
-        <a class="btn-floating btn-large blue" href="#">
+        <router-link
+            tag="a"
+            to="/record"
+            class="btn-floating btn-large blue"
+        >
           <i class="large material-icons">add</i>
-        </a>
+        </router-link>
       </div>
 
     </div>
