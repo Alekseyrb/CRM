062f10d35f32611a0ca573fbcdaa5bf1d0634ac1
Oleksii <kravchenko.a.a.rb@gmail.com>
2023-07-17 19:17:38 +0300

    add routs and views

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <div id="app">
-    <router-view/>
+    <component :is="layout">
+      <router-view/>
+    </component>
   </div>
 </template>
 
 <script>
+import EmptyLayout from "@/layouts/EmptyLayout.vue";
+import MainLayout from "@/layouts/MainLayout.vue";
+
 export default {
   name: 'App',
+  computed: {
+    layout() {
+      return (this.$route.meta.layout || 'empty') + '-layout';
+    }
+  },
+  components: {
+    MainLayout,
+    EmptyLayout,
+  }
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,62 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: '/login',
+    name: 'login',
+    meta: {layout: 'empty'},
+    component: () => import('@/views/Login.vue'),
+  },
+  {
+    path: '/register',
+    name: 'register',
+    meta: {layout: 'empty'},
+    component: () => import('@/views/Register.vue'),
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: {layout: 'main'},
+    component: () => import('@/views/Categories.vue'),
+  },
+  {
+    path: '/detail-record',
+    name: 'detail-record',
+    meta: {layout: 'main'},
+    component: () => import('@/views/DetailRecord.vue'),
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: {layout: 'main'},
+    component: () => import('@/views/History.vue'),
+  },
+  {
+    path: '/home',
     name: 'home',
-    component: HomeView
+    meta: {layout: 'main'},
+    component: () => import('@/views/Home.vue'),
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: {layout: 'main'},
+    component: () => import('@/views/Planning.vue'),
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {layout: 'main'},
+    component: () => import('@/views/Profile.vue'),
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: {layout: 'main'},
+    component: () => import('@/views/Record.vue'),
   }
 ]
 
