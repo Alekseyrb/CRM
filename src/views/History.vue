95c83a334e1c483fe71a7056c509756e1af966b8
Oleksii <kravchenko.a.a.rb@gmail.com>
2023-07-21 13:15:35 +0300

    add pie in page history

--- src/views/History.vue
+++ src/views/History.vue
@@ -5,7 +5,9 @@
     </div>
 
     <div class="history-chart">
-      <canvas></canvas>
+      <div class="pie-chart-container">
+        <Pie :data="chartData" v-if="!loading"/>
+      </div>
     </div>
 
     <Loader v-if="loading"/>
@@ -35,6 +37,10 @@
 <script>
 import paginationMixin from "@/mixins/paginationMixin";
 import HistoryTable from "@/components/HistoryTable.vue";
+import {Pie} from 'vue-chartjs'
+import {Chart as ChartJS, ArcElement, Title, Tooltip, Legend} from 'chart.js'
+
+ChartJS.register(ArcElement, Title, Tooltip, Legend)
 
 export default {
   name: "History",
@@ -42,28 +48,57 @@ export default {
   data: () => ({
     loading: true,
     records: [],
+    chartData: null,
   }),
   async mounted() {
     this.records = await this.$store.dispatch('fetchRecords');
     const categories = await this.$store.dispatch('fetchCategories');
 
-    this.setupPagination(this.records.map(record => {
-      return {
-        ...record,
-        categoryName: categories.find(c => c.id === record.categoryId).title,
-        typeClass: record.type === 'income' ? 'green' : 'red',
-        typeText: record.type === 'income' ? 'Доход' : 'Расход',
-      }
-    }));
+    this.setup(categories);
+
+    this.chartData = {
+      labels: categories.map(c => c.title),
+      datasets: [
+        {
+          label: 'Расходы по категориям',
+          backgroundColor: ['#f87979', '#00ccff', '#ffcc00', "red", "green", "yellow", "black"],
+          data: categories.map(c => {
+            return this.records.reduce((total, r) => {
+              if (r.categoryId === c.id && r.type === 'outcome') {
+                total += +r.amount
+              }
+              return total
+            }, 0)
+          })
+        }
+      ]
+    }
 
     this.loading = false;
   },
+  methods: {
+    setup(categories) {
+      this.setupPagination(this.records.map(record => {
+        return {
+          ...record,
+          categoryName: categories.find(c => c.id === record.categoryId).title,
+          typeClass: record.type === 'income' ? 'green' : 'red',
+          typeText: record.type === 'income' ? 'Доход' : 'Расход',
+        }
+      }));
+
+
+    }
+  },
   components: {
-    HistoryTable,
+    HistoryTable, Pie
   }
 }
 </script>
 
 <style scoped>
-
+.pie-chart-container {
+  width: 300px;
+  height: 300px;
+}
 </style>
