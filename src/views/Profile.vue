3626db5e35ed4b5e545f58a55bc86c6875c58200
Oleksii <kravchenko.a.a.rb@gmail.com>
2023-07-21 13:45:04 +0300

    add update name

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -4,15 +4,28 @@
       <h3>Профиль</h3>
     </div>
 
-    <form class="form">
+    <form class="form" @submit.prevent="submitHandler">
       <div class="input-field">
         <input
             id="description"
             type="text"
+            v-model="name"
+            :class="{invalid: $v.name.$dirty && !$v.name.required}"
         >
         <label for="description">Имя</label>
-        <span
-            class="helper-text invalid">name</span>
+        <small
+            class="helper-text invalid"
+            v-if="$v.name.$dirty && !$v.name.required"
+        >Введите имя</small>
+      </div>
+
+      <div class="switch">
+        <label>
+          Eng
+          <input type="checkbox" v-model="isRuLocale">
+          <span class="lever"></span>
+          Рус
+        </label>
       </div>
 
       <button class="btn waves-effect waves-light" type="submit">
@@ -24,11 +37,52 @@
 </template>
 
 <script>
+import {mapGetters, mapActions} from "vuex";
+import {required} from "vuelidate/lib/validators";
+
 export default {
-  name: "Profile"
+  name: "Profile",
+  data: () => ({
+    name: '',
+    isRuLocale: true,
+  }),
+  validations: {
+    name: {required},
+  },
+  mounted() {
+    this.name = this.info.name;
+    this.isRuLocale = this.info.locale === 'ru-Ru';
+    setTimeout(() => {
+      M.updateTextFields();
+    }, 0);
+  },
+  computed: {
+    ...mapGetters(['info']),
+  },
+  methods: {
+    ...mapActions(['updateInfo']),
+    async submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
+
+      try {
+        await this.updateInfo({
+          name: this.name,
+          locale: this.isRuLocale ? 'ru-Ru' : 'en-Us',
+        });
+        this.$message('Обновлено');
+      } catch (e) {
+        console.log(e);
+      }
+    },
+  }
 }
 </script>
 
 <style scoped>
-
+.switch {
+  margin-bottom: 2rem;
+}
 </style>
